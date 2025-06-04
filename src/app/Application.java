20b9107c3a95e422600626b92a2f1cba61fd4482
Mario Ramalho <mario@ramalho.ch>
2025-06-04 16:14:27 +0200

    Adaptation du commentaire main

---
+++ src/app/Application.java
@@ -0,0 +1,20 @@
+package app;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        // Données de base Livreurs et Bouteilles qui sont définies dans la consigne
+        
+        // new Livreur("Ducobu", "Jean", "Bern");
+        // new Livreur("Mac", "HARONI", "Fribourg");
+        // new Bouteille("Henniez rouge", 0.3, 1.0, "Suisse")
+        // new Bouteille("Henniez verte", 0.3, 1.0, "Suisse");
+        // new Bouteille("Coca-Cola", 0.5, 1.5, "USA");
+        // new Bouteille("Pepsi", 0.5, 1.5, "Italie");
+        // new Bouteille("Sinalco", 0.5, 1.5, "Suisse");
+   
+
+    }
+
+}
